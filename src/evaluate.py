a0891b8f560ea47af0def0d8d6291fafa08aa0a8
mdop297 <lenhatminh297@gmail.com>
2024-12-01 15:50:35 +0700

    pipeline added

---
+++ src/evaluate.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import pickle
+from sklearn.metrics import accuracy_score
+import yaml
+import os
+import mlflow
+from urllib.parse import urlparse
+
+
+os.environ['MLFLOW_TRACKING_URI']="https://dagshub.com/mdop297/mlpipeline.mlflow"
+os.environ['MLFLOW_TRACKING_USERNAME']="mdop297"
+os.environ["MLFLOW_TRACKING_PASSWORD"]="6d82a2ca2c42b07710d5a4d4e54ca3e648783651"
+
+
+# Load parameters from params.yaml
+params = yaml.safe_load(open("params.yaml"))["train"]
+
+def evaluate(data_path,model_path):
+    data=pd.read_csv(data_path)
+    X = data.drop(columns=["Outcome"])
+    y = data["Outcome"]
+
+    ## load the model from the disk
+    model=pickle.load(open(model_path,'rb'))
+
+    predictions=model.predict(X)
+    accuracy=accuracy_score(y,predictions)
+    ## log metrics to MLFLOW
+
+    mlflow.log_metric("accuracy",accuracy)
+    print("Model accuracy:{accuracy}")
+
+if __name__=="__main__":
+    evaluate(params["data"],params["model"])
